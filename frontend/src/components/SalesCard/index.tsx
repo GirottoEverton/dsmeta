e7cfcd286f7087c0b5cd2c58ea4e0be5248a98af
Everton Girotto <evertongirotto@yahoo.com.br>
2022-11-20 16:30:50 -0300

    Date update

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -13,17 +13,22 @@ function SalesCard() {
     const max = new Date();
 
     const [minDate, setMinDate] = useState(min);
-    const [maxdate, setMaxDate] = useState(max);
+    const [maxDate, setMaxDate] = useState(max);
 
     const [sales, setSales] = useState<Sale[]>([]);
 
     useEffect(() => {
 
-        axios.get(`${BASE_URL}/sales`)
+        const dmin = minDate.toISOString().slice(0,10);
+        const dmax = maxDate.toISOString().slice(0,10);
+
+        console.log(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             });
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
@@ -39,7 +44,7 @@ function SalesCard() {
                 </div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
-                        selected={maxdate}
+                        selected={maxDate}
                         onChange={(date: Date) => setMaxDate(date)}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
